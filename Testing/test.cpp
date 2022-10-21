d88e6406786d45275bea24047ebbc57b3d51793d
inpgbburda <bburdainpg@gmail.com>
2022-10-21 20:29:09 +0200

    Create basic test template.

---
+++ Testing/test.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( 1 == 1 );
+    REQUIRE( 2 == 2 );
+    REQUIRE( 3 == 6 );
+}
