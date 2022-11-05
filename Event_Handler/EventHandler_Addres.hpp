03898c1f85026d4cbbb04339670bd31dda36f830
Tomasz Smiech <tomasz_smiech@o2.pl>
2022-11-05 19:15:26 +0100

    Add AddressManager

---
+++ Event_Handler/EventHandler_Addres.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <string>
+
+namespace system_address
+{    
+
+enum class Priority
+{
+    Low,
+    High,
+    Emergency
+};
+
+using Address = unsigned long long int;
+
+using Parameters = struct
+{
+    uint32_t modId = 0;
+    uint16_t label = 0;
+    uint8_t priority = 0;
+    uint8_t option = 0;
+};
+
+
+class AddressManager
+{
+
+public:
+
+static Address MakeAddress(const Parameters& param)
+{
+    static_assert(sizeof(Parameters) == sizeof(Address), "Parameters' size must be equal Address size!");
+    Address tmpAddr = 0;
+
+    tmpAddr = tmpAddr & param.modId;
+    tmpAddr = tmpAddr << sizeof(param.label) & param.label;
+    tmpAddr = tmpAddr << sizeof(param.priority) & param.priority;
+    tmpAddr = tmpAddr << sizeof(param.option) & param.option;
+
+    return tmpAddr;
+}
+
+static Address MakeAddressCopy(const Address& address)
+{
+    return MakeAddress({ .modId = GetModId(address), .label = GetLabel(address), .priority = GetPriority(address), .option = GetOption(address) });
+}
+
+static std::string printOut(const Address& address)
+{
+    return std::string{ "ModuleId: "+ std::to_string(GetModId(address)) +"LabelNumber: " + std::to_string(GetLabel(address)) 
+    + "Priority: " + std::to_string(GetPriority(address)) + " | Option: " + std::to_string(GetOption(address))};
+}
+
+static typeof(Parameters::modId) GetModId(const Address& address)
+{
+    constexpr Address m_ModIdMask = 0xFFFFFFFF00000000;
+    return static_cast<typeof(Parameters::modId)>(address & m_ModIdMask);
+}
+
+static typeof(Parameters::label) GetLabel(const Address& address)
+{
+    constexpr Address m_labelMask = 0x00000000FFFF0000;
+    return static_cast<typeof(Parameters::label)>(address & m_labelMask);
+}
+
+static typeof(Parameters::priority) GetPriority(const Address& address)
+{
+    constexpr Address m_prorityMask = 0x000000000000FF00;
+    return static_cast<typeof(Parameters::priority)>(address & m_prorityMask);
+}
+
+static typeof(Parameters::option) GetOption(const Address& address)
+{
+    constexpr Address m_optionMask = 0x00000000000000FF;
+    return static_cast<typeof(Parameters::option)>(address & m_optionMask);
+}
+
+static void SetModId(Address& address, const typeof(Parameters::modId) modId)
+{
+    Address tmpAddr = address;
+    address = MakeAddress({.modId = modId, .label = GetLabel(address), .priority = GetPriority(address), .option = GetOption(address)});
+}
+
+static void SetLabel(Address& address, const typeof(Parameters::label) label)
+{
+    Address tmpAddr = address;
+    address = MakeAddress({.modId = GetPriority(address), .label = label, .priority = GetPriority(address), .option = GetOption(address)});
+}
+
+static void SetPriority(Address& address, const typeof(Parameters::priority) priority)
+{
+    Address tmpAddr = address;
+    address = MakeAddress({.modId = GetPriority(address), .label = GetLabel(address), .priority = priority, .option = GetOption(address)});
+}
+
+static void SetOption(Address& address, const typeof(Parameters::option) option)
+{
+    Address tmpAddr = address;
+    address = MakeAddress({.modId = GetPriority(address), .label = GetLabel(address), .priority = GetPriority(address), .option = option});
+}
+
+};
+
+}
