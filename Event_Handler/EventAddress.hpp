58d05ac33170976ed0cf67c219f318030eddd835
Tomasz Smiech <tomasz_smiech@o2.pl>
2022-11-06 22:03:15 +0100

    Add AddressLabels.hpp and UT

--- Event_Handler/EventAddress.hpp
+++ Event_Handler/EventAddress.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <string>
+#include <unistd.h>
+#include <iostream>
+
+#include "AddressLabels.hpp"
 
 namespace system_address
 {    
@@ -28,22 +32,23 @@ class AddressManager
 
 public:
 
+static Address MakeAddress()
+{   
+    return MakeAddress({});
+}
+
 static Address MakeAddress(const Parameters& param)
 {
     static_assert(sizeof(Parameters) == sizeof(Address), "Parameters' size must be equal Address size!");
+    constexpr int bitOffset = 8;
+
     Address tmpAddr = 0;
-
-    tmpAddr = tmpAddr & param.modId;
-    tmpAddr = tmpAddr << sizeof(param.label) & param.label;
-    tmpAddr = tmpAddr << sizeof(param.priority) & param.priority;
-    tmpAddr = tmpAddr << sizeof(param.option) & param.option;
-
+    
+    tmpAddr = tmpAddr | static_cast<Address>(param.modId);
+    tmpAddr = tmpAddr << sizeof(param.label)*bitOffset | static_cast<Address>(param.label);
+    tmpAddr = tmpAddr << sizeof(param.priority)*bitOffset | static_cast<Address>(param.priority);
+    tmpAddr = tmpAddr << sizeof(param.option)*bitOffset | static_cast<Address>(param.option);
     return tmpAddr;
-}
-
-static Address MakeAddressCopy(const Address& address)
-{
-    return MakeAddress({ .modId = GetModId(address), .label = GetLabel(address), .priority = GetPriority(address), .option = GetOption(address) });
 }
 
 static std::string printOut(const Address& address)
@@ -55,19 +60,22 @@ static std::string printOut(const Address& address)
 static typeof(Parameters::modId) GetModId(const Address& address)
 {
     constexpr Address m_ModIdMask = 0xFFFFFFFF00000000;
-    return static_cast<typeof(Parameters::modId)>(address & m_ModIdMask);
+    constexpr int bitShift = 32;
+    return static_cast<typeof(Parameters::modId)>((address & m_ModIdMask) >> bitShift);
 }
 
 static typeof(Parameters::label) GetLabel(const Address& address)
 {
     constexpr Address m_labelMask = 0x00000000FFFF0000;
-    return static_cast<typeof(Parameters::label)>(address & m_labelMask);
+    constexpr int bitShift = 16;
+    return static_cast<typeof(Parameters::label)>((address & m_labelMask) >> bitShift);
 }
 
 static typeof(Parameters::priority) GetPriority(const Address& address)
 {
     constexpr Address m_prorityMask = 0x000000000000FF00;
-    return static_cast<typeof(Parameters::priority)>(address & m_prorityMask);
+    constexpr int bitShift = 8;
+    return static_cast<typeof(Parameters::priority)>((address & m_prorityMask) >> bitShift);
 }
 
 static typeof(Parameters::option) GetOption(const Address& address)
@@ -98,6 +106,16 @@ static void SetOption(Address& address, const typeof(Parameters::option) option)
 {
     Address tmpAddr = address;
     address = MakeAddress({.modId = GetPriority(address), .label = GetLabel(address), .priority = GetPriority(address), .option = option});
+}
+
+static typeof(Parameters::label) LabelToParam(const AddressLabels label)
+{
+    return static_cast<typeof(Parameters::label)>(label);
+}
+
+static uint32_t GetPID()
+{
+    return static_cast<uint32_t>(getppid());
 }
 
 };
