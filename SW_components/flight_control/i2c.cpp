63a6771089ccc76844509c1a6a4567e909bc3dc2
inpgbburda <bburdainpg@gmail.com>
2023-03-29 21:27:48 +0200

    Add i2c UT builds to gitignore

--- SW_components/flight_control/i2c.cpp
+++ SW_components/flight_control/i2c.cpp
@@ -1,8 +1,9 @@
 extern "C" {
   #include <linux/i2c-dev.h>
+#ifndef _UNIT_TEST 
   #include <i2c/smbus.h>
+#endif /* _UNIT_TEST */
 }
-
 
 #include <fcntl.h>    /* For O_RDWR */
 #include <unistd.h>   /* For open(), creat() */
@@ -12,20 +13,24 @@ extern "C" {
 
 
 #include <stdint.h>
+#include <stdexcept>
 #include "i2c_cfg.hpp"
+
+#define I2C_DRV_DESRC_MAX_FILE_L 20
 
 static int I2c_File_Dcr = 0;
 
 static uint8_t I2c_Read_Byte(uint8_t reg_address);
-static uint8_t ComposeDriverFilename(char* filename, int adapter_nr);
+static void ComposeDriverFilename(char* filename, int adapter_nr);
+// static void SetSlaveAddr();
+static void DriverInit(int driver_num);
 
 void i2c_main_fun(void)
 {
     int adapter_nr = I2C_KERNEL_ADAPTER_NUM; /* probably dynamically determined */
     char filename[I2C_KERNEL_FILENAME_MAX_SIZE];
 
-
-    snprintf(filename, 19, "/dev/i2c-%d", adapter_nr); /* (buffer to safe string; max buffer size in bytes; filename with var <d>; var) */
+    ComposeDriverFilename(filename, adapter_nr);
     I2c_File_Dcr = open(filename, O_RDWR); /* Open file read/write access*/
     if (I2c_File_Dcr < 0) {
         /* ERROR HANDLING; you can check errno to see what went wrong */
@@ -47,16 +52,14 @@ void i2c_main_fun(void)
     
 }
 
-static uint8_t ComposeDriverFilename(char* filename, int adapter_nr)
-{
-    return -1;
-}
 
 static uint8_t I2c_Read_Byte(uint8_t reg_address)
 {
   int32_t result;
   uint8_t read_data;
+#ifndef _UNIT_TEST 
   result = i2c_smbus_read_byte_data(I2c_File_Dcr, reg_address);
+#endif
   if (result < 0) {
      /* ERROR HANDLING: i2c transaction failed */
      read_data = 0U;
@@ -66,5 +69,19 @@ static uint8_t I2c_Read_Byte(uint8_t reg_address)
   return result;
 }
 
+static void ComposeDriverFilename(char* filename, int adapter_nr)
+{
+    if( (nullptr != filename) && (adapter_nr >= 0) && (adapter_nr < 10) )
+    {
+        snprintf(filename, I2C_DRV_DESRC_MAX_FILE_L, "/dev/i2c-%d", adapter_nr);
+    }
+    else
+    {
+        throw "out of range";
+    }
+}
 
-
+static void DriverInit(int driver_num)
+{
+    
+}
--- SW_components/flight_control/i2c_UT/i2c_UT.cpp
+++ SW_components/flight_control/i2c_UT/i2c_UT.cpp
@@ -1,9 +1,36 @@
 #define CATCH_CONFIG_MAIN
 
 #include <catch2/catch_all.hpp>
+#include "i2c.cpp"
 
 
+TEST_CASE( "Test generating file descriptor", "[i2c]" ) 
+{
+    using Catch::Matchers::Equals;
+    char filename[I2C_DRV_DESRC_MAX_FILE_L];
 
-TEST_CASE( "Test generating file descriptor", "[i2c]" ) {
+    ComposeDriverFilename(filename, 1);
+    std::string filename_str(filename);
+
+    REQUIRE_THAT(filename_str, Equals("/dev/i2c-1"));
+
+}
+
+TEST_CASE( "Test failing generating file descriptor - nullptr instead of string", "[i2c]" ) 
+{
+    char *filename = nullptr;
+    REQUIRE_THROWS( ComposeDriverFilename(filename, 1) );
+}
+
+TEST_CASE( "Driver number out of range", "[i2c]" ) 
+{
+    char filename[I2C_DRV_DESRC_MAX_FILE_L];
+    REQUIRE_THROWS( ComposeDriverFilename(filename, 10) );
+    REQUIRE_THROWS( ComposeDriverFilename(filename, -3) );
+}
+
+TEST_CASE( "Driver initialization", "[i2c]" ) 
+{
+    DriverInit(1);
 
 }
