aaea55ea8676ca00d614a52133faeab7f5b398a1
inpgbburda <49471138+inpgbburda@users.noreply.github.com>
2023-06-10 14:48:23 +0200

    Configure UT environment for mpu6050 module

---
+++ SW_components/03_IoHwab/mpu6050/mpu6050_UT/mpu6050_UT.cpp
@@ -0,0 +1,15 @@
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+#include "mpu6050.hpp"
+
+TEST_GROUP(I2c)
+{
+    void setup()
+    {
+    }
+    void teardown()
+    {
+
+    }
+};
+
