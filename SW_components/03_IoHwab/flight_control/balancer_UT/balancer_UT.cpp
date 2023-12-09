d3a2ee261c6f6c575600320862dec96583f238ee
inpgbburda <49471138+inpgbburda@users.noreply.github.com>
2023-12-09 12:03:58 +0100

    Create method to set base thrust
    Base thrust is needed to provide bias, to wchich control
    values will be added. It assures, that even if no control is
    needed in a given moment, the motors still will spin evenly
    with given value.
    
    - add methods and unit test
    - add block comments for balancer file

--- SW_components/03_IoHwab/flight_control/balancer_UT/balancer_UT.cpp
+++ SW_components/03_IoHwab/flight_control/balancer_UT/balancer_UT.cpp
@@ -4,6 +4,17 @@
 #include "balancer.hpp"
 #include "pwm.hpp"
 
+/*
+*    Testing constant configuration
+*/
+
+const int32_t Inital_Base_Thrust = 0;
+const int32_t Base_Thrust = 25;
+
+
+/*
+*    Test scenarios
+*/
 TEST_GROUP(Balancer)
 {
     Balancer* balancer;
@@ -31,4 +42,18 @@ TEST(Balancer, CalculatesControlSignalForYaw)
         .withParameter("pwm_percentage", 25);
 
     balancer->ProcessControl();
+}
+
+TEST(Balancer, SetsBaseThrust)
+{
+    int thrust_1 = balancer->GetCurrentThrust(MOTOR_1);
+    int thrust_2 = balancer->GetCurrentThrust(MOTOR_2);
+
+    CHECK_EQUAL(thrust_1, Inital_Base_Thrust);
+    CHECK_EQUAL(thrust_2, Inital_Base_Thrust);
+
+    balancer -> SetBaseThrust(Base_Thrust);
+
+    CHECK_EQUAL(balancer->GetCurrentThrust(MOTOR_1), Base_Thrust);
+    CHECK_EQUAL(balancer->GetCurrentThrust(MOTOR_2), Base_Thrust);
 }
